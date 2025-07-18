aa93ff2f5f654ca32c6964099d2b18180e205df0
Aditya Mandala <96357344+aditya234a@users.noreply.github.com>
2025-07-18 17:03:44 +0000

    Practicewok

--- Practicework/day.py
+++ Practicework/day.py
@@ -118,17 +118,22 @@ for row in range(9):
         print('*',end = " ")
     print()
 
+    n = int(input("enter the number:"))
 
-n = int(input("enter the value:"))
 for row in range(n):
-    if row<n/2:
-        for col in range (row+1):
-            print("*",end = " ")
+    if row<5:
+        for spa in range(n-row-1):
+            print(' ',end=' ')
         print()
     else:
-        for col1 in range(n - row):
-            print("*",end = " ")
-        print()
+        for col in range (n-1):
+
+            for spa in range(col-1 ):
+
+                print('*',end=' ')
+            print()
+            
+
 
 
 n = int(input("enter the number:"))
@@ -145,32 +150,102 @@ for row in range(n):
 
                 print('*',end=' ')
             print()
-            '''
+            
 
-n = int(input("enter the value:"))
 
-for row in range(n):
-    for spa in range(row):
-        print(' ',end = ' ')
-    for col in range(n-row):
-        print("*",end=" ")
+n1 = int(input("enter the value:"))                          #
+for row in range(n1):                                        #
+    if row<n1/2:                                             #
+        for col in range (row+1):                            #
+            print("*",end = " ")                             #
+        print()                                              #
+    else:                                                    #
+        for col1 in range(n1 - row):                         #
+            print("*",end = " ")                             #
+        print()                                              #
+
+
+
+
+n2 = int(input("enter the value:"))                       
+                                                          
+for row in range(n2):                                      #     * * * * *
+    for spa in range(row):                                 #       * * * *
+        print(' ',end = ' ')                               #         * * *
+    for col in range(n2-row):                              #           * *
+        print("*",end=" ")                                 #             *
+    print()#
+
+           
+
+n = int(input("enter the size:"))                           #    * * * * * 
+for row in range(n):                                        #      * * * *
+    print(' '*row,end=' ')                                  #        * * *
+    print("*"*(n-row),end=' ')                              #          * *
+    print( )                                                #            *
+
+
+n = int(input("enter the size:"))
+
+for row in range (n):
+    if row<=n//2:
+        print('*'*(row+1),end='')
+    else:
+        print('*'*(n-row+1),end='')
     print()
+    
+
+n = int(input("enter the size:"))                        #   *******
+for row in range(n):                                     #   *     *
+    for col in range(n):                                 #   *     *
+        if row==0 or row==n-1 or col==0 or col==n-1:     #   *     *
+            print("*",end="")                            #   *     *
+        else:                                            #   *     *
+            print(" ",end="")                            #   *     *
+    print()                                              #   *******
+  
+n = int(input("enter the size:"))                                                     #   *******
+for row in range(n):                                                                  #   *  *  *
+    for col in range(n):                                                              #   *  *  *
+        if row==0 or row==n-1 or col==0 or col==n-1 or row==n//2 or col==n//2:        #   *******
+            print("*",end="")                                                         #   *  *  *
+        else:                                                                         #   *  *  *
+            print(" ",end="")                                                         #   *******
+    print()        
+
+n = int(input("enter the size:"))                                 #   *           *
+for row in range(n):                                              #     *       *
+    for col in range(n):                                          #       *   *
+        if row==col or row+col==n-1:                              #         *
+            print("*",end="")                                     #      *     *
+        else:                                                     #     *        *
+            print(" ",end="")                                     #    *          *
+    print()                                                        
+    
+n = int(input("enter the size:"))                                 #   *           *
+for row in range(n):                                              #     *       *
+    for col in range(n):                                          #       *   *
+        if row==col or row==n-1 or col==n//2:                        #         *
+            print("*",end="")                                     #      *     *
+        else:                                                     #     *        *
+            print(" ",end="")
 
 
 
 
 
+row==0 or row==n-1 or row==n//2 or (col==0 and row<=n//2) or (col==n-1 and row>=n//2):  # S
 
+row==0 or row==n//2 or col==0 or col==n-1:  # A
 
-
-
-
-
+col==0
 '''
-    for row in range(n):
-    for spa in range(n-row-1):
-        print(' ',end=' ')
-    for col in range(row+1):
-        print('*',end=' ')
+
+n = int(input("enter the size:"))                                 
+for row in range(n):                                              
+    for col in range(n):                                          
+        if (col==0 or col==n-1) and row<=n//2 or (row-col==n//2 and row>=n//2) :   
+            print("*",end="")
+        else:
+            print(" ",end="")
     print()
-    '''
