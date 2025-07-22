dc1432fb359940aa8e6e8ea9c179bf7ab741eb2a
Aditya Mandala <96357344+aditya234a@users.noreply.github.com>
2025-07-22 15:55:38 +0000

    classwork

---
+++ classwork/14. functions.py
@@ -0,0 +1,43 @@
+data = {'current_balance':46578,'history':[]}
+def check_balance():
+    print(f"Current Balance: {data['current_balance']}")
+def deposit():
+    amount = int(input("enter the amount:"))
+    data['current_balance']+=amount
+    data['history'].append(f'Deposited - {amount}')
+    print(f'Deposited Successfully - {amount}')
+def withdraw():
+    amount = int(input("enter the amount:"))
+    if data['current_balance']>=amount:
+        data['current_balance']-=amount
+        data['history'].append(f'withdraw - (amount)')
+        print(f'Withdraw Successfully - {amount}')
+    else:
+        print("Ins blnc")
+def history():
+    print("Transactions")
+    for i in data['history']:
+        print(i)
+
+while True:
+    print('Welcome to ATM'.center(30,'*'))
+    print('1.Check Balance')
+    print('2.Deposit')
+    print('3.Withdraw')
+    print('4.History')
+    print('0.Exit')
+
+    op = int(input("enter your choice:"))
+    if op==0:
+        print("======Thnakyou======")
+        break
+    elif op==1:
+        check_balance()
+    elif op==2:
+        deposit()
+    elif op==3:
+        withdraw()
+    elif op==4:
+        history()
+    else:
+        print("Invaild choice.Try Again")        
