1cba502742eb02e76d6abc59f26b7dea20f49dda
aditya234a <adityamandala647@gmail.com>
2025-08-01 12:34:27 +0530

    Practicework

--- Practicework/rquestions.py
+++ Practicework/rquestions.py
@@ -7,12 +7,37 @@ def reverse(s,ind):
     
 s = input("enter the string:")
 reverse(s,0)
-'''
+
 
 def sumofdigits(n):
     if n == 0:
-        return 0
+        return n
     return n % 10 + sumofdigits(n//10)
 
 n = int(input("enter the number:"))
 print("sum of digits is:", sumofdigits(n))
+
+
+def productofdigits(n):
+    if n ==0:
+        return 2
+    return n%10 * productofdigits(n//10)
+
+n =int(input("enter the number:"))
+print("product of digits is:",productofdigits(n))
+
+
+def addnumbers(n):
+    if n ==0:
+        return 0
+    return n%10 + addnumbers(n//10)
+n = int(input("enter the numbers:"))
+print("add the two numbers:",addnumbers(n))
+'''
+
+def countnumbers(n):
+    if n<10:
+        return 1
+    return 1 + countnumbers(n//10)
+n = int(input("enter the numbers:"))
+print("count the numbers:",countnumbers(n))
